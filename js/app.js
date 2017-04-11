ce9570378e41b4ff377b5258575c04c325a33d9d
Andre Carapia <carapia.andre@gmail.com>
2017-04-10 22:53:37 -0300

    feat: Player Move Functions

--- js/app.js
+++ js/app.js
@@ -1,3 +1,8 @@
+//Just to simplify console.log when developing
+var log = function(msg) {
+  console.log(msg);
+};
+
 // Enemies our player must avoid
 var Enemy = function() {
     // Variables applied to each of our instances go here,
@@ -25,8 +30,8 @@ Enemy.prototype.render = function() {
 // This class requires an update(), render() and
 // a handleInput() method.
 var Player = function() {
-  this.x = 255;
-  this.y = 255;
+  this.x = 200;
+  this.y = 390;
   this.sprite = 'images/char-boy.png';
 };
 
@@ -40,6 +45,58 @@ Player.prototype.render = function() {
 
 Player.prototype.handleInput = function(key) {
 
+  var xLen = 101,
+      yLen = 83;
+
+  if(key != undefined){
+    if(isX()){
+      moveX();
+    }
+    else{
+      moveY();
+    }
+  }
+
+  function isX(){
+    return key === 'right' || key === 'left' ?
+      true :
+      false;
+  }
+
+  function moveX(){
+    if(player.x > minX && player.x < maxX){
+      key === 'right' ?
+        player.x += xLen :
+        player.x += -xLen;
+    }
+  }
+
+  function moveY(){
+    key === 'down' ?
+      player.y += yLen :
+      player.y += -yLen;
+  }
+
+  // function isOnLimit(axisX){
+  //   //minX = -2 | maxX = 402
+  //   //minY = 390 | maxY = -25
+  //
+  //   var maxX = 402,
+  //       minX = -2,
+  //       maxY = 390,
+  //       minY = -25;
+  //
+  //   if(axisX === true){
+  //       var newPos = key === 'right' ?
+  //         player.x += xLen :
+  //         player.x += -xLen;
+  //
+  //       return newPos > minX && newPos < maxX ?
+  //
+  //
+  //   }
+  //   positionX = player.x + xLen
+  // }
 };
 
 // Now instantiate your objects.
@@ -48,8 +105,6 @@ Player.prototype.handleInput = function(key) {
 var player = new Player();
 
 var allEnemies = [new Enemy(), new Enemy(), new Enemy()];
-
-
 
 // This listens for key presses and sends the keys to your
 // Player.handleInput() method. You don't need to modify this.
