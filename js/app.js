d4f0724c39d4088a200c031ae61d2c0f3a6dff52
Andre Carapia <carapia.andre@gmail.com>
2017-04-18 16:07:43 -0300

    feat: first bug(enemy) moviment =D

--- js/app.js
+++ js/app.js
@@ -7,6 +7,8 @@ var log = function(msg) {
 var Enemy = function() {
     // Variables applied to each of our instances go here,
     // we've provided one for you to get started
+    this.x = 10;
+    this.y = 60;
 
     // The image/sprite for our enemies, this uses
     // a helper we've provided to easily load images
@@ -19,6 +21,7 @@ Enemy.prototype.update = function(dt) {
     // You should multiply any movement by the dt parameter
     // which will ensure the game runs at the same speed for
     // all computers.
+    this.x += (50 * dt);
 };
 
 // Draw the enemy on the screen, required method for game
@@ -112,10 +115,10 @@ Player.prototype.handleInput = function(key) {
 
 // Now instantiate your objects.
 // Place all enemy objects in an array called allEnemies
+var allEnemies = [new Enemy(), new Enemy(), new Enemy()];
+
 // Place the player object in a variable called player
 var player = new Player();
-
-var allEnemies = [new Enemy(), new Enemy(), new Enemy()];
 
 // This listens for key presses and sends the keys to your
 // Player.handleInput() method. You don't need to modify this.
